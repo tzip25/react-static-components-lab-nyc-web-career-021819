859c1656f46f4097f13d403da6ebeab68537d651
github username <github email address>
2019-04-20 17:35:54 -0400

    Done.

--- src/EinsteinQuoteComponent.js
+++ src/EinsteinQuoteComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
-export class EinsteinQuoteComponent extends Component {
+export default class EinsteinQuoteComponent extends Component {
+
 	render() {
 		return (
 			<div className="bar" id="einstein">
